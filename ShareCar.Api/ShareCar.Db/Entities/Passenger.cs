97c739154cf9153d6327c73afb89c103aa4f1e1a
Elena Reivytyte <e.reivytyte@gmail.com>
2018-07-19 15:50:25 +0300

    vol. 2

---
+++ ShareCar.Api/ShareCar.Db/Entities/Passenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShareCar.Db.Entities
+{
+    public class Passenger
+    {
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int RideId { get; set; }
+        public Ride Ride { get; set; }
+        public bool Completed{ get; set; }
+    }
+}
