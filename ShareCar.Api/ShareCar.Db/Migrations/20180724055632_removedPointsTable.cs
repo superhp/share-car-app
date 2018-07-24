b4e91091060da8d97d52c147748a26db4d17952d
Elena Reivytyte <e.reivytyte@gmail.com>
2018-07-24 11:18:45 +0300

    moved DI folder

---
+++ ShareCar.Api/ShareCar.Db/Migrations/20180724055632_removedPointsTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ShareCar.Db.Migrations
+{
+    public partial class removedPointsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Rides",
+                newName: "RideDateTime");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RideDateTime",
+                table: "Rides",
+                newName: "DateTime");
+        }
+    }
+}
