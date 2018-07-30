a7aad2d3f721129e2c1f5671f94945f2c3693e45
CTS\717358 <g.dedinaite@gmail.com>
2018-07-30 10:51:55 +0300

    fixed isues with https

--- ShareCar.Client/src/helpers/axiosHelper.js
+++ ShareCar.Client/src/helpers/axiosHelper.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import history from './history';
 
 const api = axios.create({
-    baseURL: `http://localhost:5963/api/`,
+    baseURL: `https://localhost:44360/api/`,
     withCredentials: true
 });
 
