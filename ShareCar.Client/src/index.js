653004b70a3edd1e31cc02c1be0ae32ba57b4597
Kasparas Taminskas <kasparas.taminskas@yahoo.com>
2018-07-30 15:56:37 +0300

    Implemented basic UserProfile component with data fetch from mock API controller method

--- ShareCar.Client/src/index.js
+++ ShareCar.Client/src/index.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Route, Switch } from 'react-router-dom';
-import { Router } from 'react-router';
-import history from './helpers/history';
-import Layout from './components/common/Layout';
-import Login from './components/Login';
-import MyProfile from './components/MyProfile';
-import RoleSelection from './components/RoleSelection';
-import Driver from './components/Driver';
-import Passenger from './components/Passenger';
-//import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Route, Switch } from "react-router-dom";
+import { Router } from "react-router";
+import history from "./helpers/history";
+import Layout from "./components/common/Layout";
+import Login from "./components/Login";
+import MyProfile from "./components/MyProfile";
+import RoleSelection from "./components/RoleSelection";
+import Driver from "./components/Driver";
+import Passenger from "./components/Passenger";
+import UserProfile from "./components/UserProfile";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 ReactDOM.render(
-    <Router history={history} >
-        <Switch>
-            <Route path='/login' component={ Login } />
-            <Layout>
-                <Route exact path='/' component={ RoleSelection } />
-                <Route path='/driver' component={ Driver } />
-                <Route path='/passenger' component={ Passenger } />
-
-            </Layout>
-        </Switch>
-    </Router>,
-    document.getElementById('root')
+  <Router history={history}>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Layout>
+        <Route exact path="/" component={RoleSelection} />
+        <Route path="/driver" component={Driver} />
+        <Route path="/passenger" component={Passenger} />
+        <Route path="/profile" component={UserProfile} />
+      </Layout>
+    </Switch>
+  </Router>,
+  document.getElementById("root")
 );
