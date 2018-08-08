1e904114dea7bdfffeba1699fda69354bb6e2dd1
CTS\717358 <g.dedinaite@gmail.com>
2018-08-08 15:22:28 +0300

    prepare to change role

--- ShareCar.Client/src/components/NavBar.jsx
+++ ShareCar.Client/src/components/NavBar.jsx
@@ -1,23 +1,24 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
+import {RoleContext}  from "../helpers/roles";
 
 export default class NavBar extends React.Component<{}> {
   render() {
     return (
       <div className="navBar">
-        <Link className="navBar-button" to="/profile">
-          <button className="btn btn-primary">Profile</button>
-        </Link>
-        <Link className="navBar-button" to="/routes">
-          <button className="btn btn-primary">Routes map</button>
-        </Link>
-        <Link className="navBar-button" to="/rides">
-          <button className="btn btn-primary">Rides</button>
-        </Link>
-        <Link className="navBar-button" to="/">
-          <button className="btn btn-primary">Change role</button>
-        </Link>
+      <Link className="navBar-button" role="button" to="/profile">Profile</Link>
+    
+        <Link className="navBar-button" role="button" to="/routes">Routes map</Link>
+        <RoleContext.Consumer>
+      {
+        role => (
+      role === "driver"
+      ? <Link className="navBar-button" role="button" to="/riderequest">Requests</Link>
+      : <Link className="navBar-button" role="button" to="/rides">Rides</Link>
+      )}
+    </RoleContext.Consumer>
+        <Link className="navBar-button" role="button" to="/">Change role</Link>
       </div>
     );
   }
