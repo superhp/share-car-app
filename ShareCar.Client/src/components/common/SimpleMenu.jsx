0a03eef1e661eb0f23008d2aa114591e1670bdd1
Kasparas Taminskas <kasparas.taminskas@yahoo.lt>
2018-08-11 15:44:23 +0300

    Styled displayRoutes component. Implemented Select Office integration.

---
+++ ShareCar.Client/src/components/common/SimpleMenu.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { withStyles } from "@material-ui/core/styles";
+import { OfficeAddresses } from "../AddressData";
+
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  paper: {
+    marginRight: theme.spacing.unit * 2
+  }
+});
+
+class SimpleMenu extends React.Component {
+  state = {
+    open: false
+  };
+
+  handleToggle = () => {
+    this.setState(state => ({ open: !state.open }));
+  };
+
+  handleClose = (event, index) => {
+    if (this.anchorEl.contains(event.target)) {
+      return;
+    }
+
+    this.setState({ open: false });
+    this.props.handleSelection(event, index, this.props.whichButton);
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <div>
+          <Button
+            variant="contained"
+            className="select-office-menu"
+            buttonRef={node => {
+              this.anchorEl = node;
+            }}
+            aria-owns={open ? "menu-list-grow" : null}
+            aria-haspopup="true"
+            onClick={this.handleToggle}
+          >
+            Or Select Office
+          </Button>
+          <Popper
+            classsName="list-of-items"
+            open={open}
+            style={{ zIndex: 999999 }}
+            anchorEl={this.anchorEl}
+            transition
+            disablePortal
+          >
+            {({ TransitionProps, placement }) => (
+              <Grow
+                {...TransitionProps}
+                id="menu-list-grow"
+                style={{ zIndex: 999999 }}
+                style={{
+                  transformOrigin:
+                    placement === "bottom" ? "center top" : "center bottom"
+                }}
+              >
+                <Paper style={{ zIndex: 999999 }}>
+                  <ClickAwayListener onClickAway={this.handleClose}>
+                    <MenuList style={{ zIndex: 999999 }}>
+                      {OfficeAddresses.map((element, index) => (
+                        <MenuItem
+                          key={index}
+                          style={{ zIndex: 999999 }}
+                          onClick={e => this.handleClose(e, index)}
+                        >
+                          {element.number} {element.street} {element.city}
+                        </MenuItem>
+                      ))}
+                    </MenuList>
+                  </ClickAwayListener>
+                </Paper>
+              </Grow>
+            )}
+          </Popper>
+        </div>
+      </div>
+    );
+  }
+}
+
+SimpleMenu.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(SimpleMenu);
