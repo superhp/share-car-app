c699526060d1061b0dd4abefa19ae58746098aa5
DESKTOP-1LV5153\player <edgar.reis447@gmail.com>
2019-03-07 16:15:24 +0200

    user manual component

---
+++ ShareCar.Client/src/components/Manual.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Route, Link } from "react-router-dom";
+
+import "../styles/genericStyles.css";
+
+class Manual extends React.Component {
+    render(){
+        return (
+            <div> MMMMMMMMMMM</div>
+           );
+    }
+};
+export default Manual;
