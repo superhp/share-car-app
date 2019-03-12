ec3f71847fe30bb6a22d8a672aedd82fce857838
DESKTOP-1LV5153\player <edgar.reis447@gmail.com>
2019-03-12 10:28:24 +0200

    Merge branch 'dev' into feature/OSRM-generated-route-editing
    # Conflicts:
    #	ShareCar.Client/src/components/Driver/DriverMap.jsx
    #	ShareCar.Client/src/components/Driver/Map/DriverRouteInput.jsx

---
+++ ShareCar.Api/ShareCar.Logic/Exceptions/NoSeatsInRideException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShareCar.Logic.Exceptions
+{
+    public class NoSeatsInRideException : Exception
+    {
+    }
+}
---
+++ ShareCar.Client/src/components/Passenger/Route/DriverRouteSuggestionsItem.jsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Phone from "@material-ui/icons/Phone";
+import Button from "@material-ui/core/Button";
+import Timer from "@material-ui/icons/Timer";
+
+import "../../../styles/genericStyles.css";
+import "../../../styles/testmap.css";
+
+export const DriverRouteSuggestionsItem = props => (
+    <Grid
+        className="driver-button-container"
+        item xs={12}
+    >
+        <Grid 
+            container 
+            alignItems="center" 
+            justify="center" 
+            className="names-and-phones" 
+            item xs={12}
+        >
+            <Grid container className="driver-name" item xs={12}>
+                <Typography variant="caption">Driver </Typography>
+                <Typography variant="body1">
+                    {props.ride.driverFirstName} {props.ride.driverLastName}
+                </Typography>
+            </Grid>
+            <Grid className="call" item xs={12}>
+                <Phone />
+                <Typography variant="body1">
+                {props.ride.driverPhone}
+                </Typography>
+            </Grid>
+            <Grid item xs={12}>
+                <Timer />
+                <Typography>{props.ride.rideDateTime}</Typography>
+            </Grid>
+        </Grid>
+        <Button
+            color="primary"
+            variant="contained"
+            onClick={() => props.onRegister()}
+        >
+            Register
+        </Button>
+    </Grid>
+);
