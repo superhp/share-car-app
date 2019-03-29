0271c0d6564025fa3d23aad9d8805ea4a5ffd4b4
Gerda Simoliunaite <gerda.simoliunaite@storebrand.no>
2019-03-29 16:10:25 +0200

    Styled modal and calendar

--- ShareCar.Client/src/components/Driver/Ride/RideSchedulerHelper.jsx
+++ ShareCar.Client/src/components/Driver/Ride/RideSchedulerHelper.jsx
@@ -6,54 +6,73 @@ import InfiniteCalendar, {
 } from "react-infinite-calendar";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
+import Dialog from "@material-ui/core/Dialog";
 import Toolbar from "@material-ui/core/Toolbar";
+import Slide from "@material-ui/core/Slide";
 import Grid from "@material-ui/core/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Button from "@material-ui/core/Button";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 import TimePickers from "../../common/TimePickers";
 
+import "../../../styles/newRideForm.css";
+
 const today = new Date();
 
-export const RideSchedulerHelper = (props) => (
-    <Grid container justify="center">
-        <AppBar className={props.appBar}>
-            <Toolbar>
-                <IconButton
-                    color="inherit"
-                    onClick={() => props.handleClose()}
-                    aria-label="Close"
-                >
-                    <CloseIcon />
-                </IconButton>
-                <Typography
-                    variant="subheading"
-                    color="inherit"
-                    className={props.flex}
-                >
-                    Schedule Your Rides
-                </Typography>
-                <Button
-                    disabled={props.selectedDates.length === 0 ? true : false}
-                    variant="contained"
-                    color="inherit"
-                    onClick={() => props.handleCreate()}
-                >
-                    Create Rides
-                </Button>
-            </Toolbar>
-        </AppBar>
-        <InfiniteCalendar
-            onSelect={e => props.handleSelect(e)}
-            Component={withMultipleDates(Calendar)}
-            selected={props.selectedDates}
-            interpolateSelection={defaultMultipleDateInterpolation}
-            width={375}
-            height={380}
-            disabledDays={[0, 6]}
-            minDate={today}
-        />
-        <TimePickers onTimeSet={value => props.handleTime(value)} />
-    </Grid>
-);
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
+class RideSchedulerHelper extends React.Component {
+    state = {
+        open: true
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    render() {
+        return(
+            <Dialog
+                open={this.state.open}
+                onClose={() => this.handleClose()}
+                TransitionComponent={Transition}
+            >
+                <DialogTitle >Schedule Your Rides</DialogTitle>
+                <Grid container>
+                    <Grid item xs={12} className="calendar-container">
+                        <InfiniteCalendar
+                            onSelect={e => this.props.handleSelect(e)}
+                            Component={withMultipleDates(Calendar)}
+                            selected={this.props.selectedDates}
+                            interpolateSelection={defaultMultipleDateInterpolation}
+                            width={375}
+                            height={380}
+                            disabledDays={[0, 6]}
+                            minDate={today}
+                            className="calendar"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TimePickers onTimeSet={value => this.props.handleTime(value)} />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button
+                            disabled={this.props.selectedDates.length === 0 ? true : false}
+                            variant="contained"
+                            color="inherit"
+                            onClick={() => this.props.handleCreate()}
+                        >
+                            Create Rides
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Dialog>
+        );
+    }
+}
+
+export default RideSchedulerHelper;
