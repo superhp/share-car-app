a333cce892ec54fe2e4951472ad181900bd5d52b
Aurimas Šimkus <saurimas@hotmail.com>
2019-04-18 16:27:12 +0300

    Environment setting added

--- ShareCar.Client/src/helpers/axiosHelper.js
+++ ShareCar.Client/src/helpers/axiosHelper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import history from "./history";
 
 const api = axios.create({
-  baseURL: `https://localhost:44347/api/`,
+  baseURL: process.env['API_URL'] || `https://localhost:44347/api/`,
   withCredentials: true
 });
 
